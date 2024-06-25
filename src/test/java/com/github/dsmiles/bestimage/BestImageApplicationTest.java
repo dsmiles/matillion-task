5897500167e33b8743acb0315cefc92698713f72
David Smiles <david.smiles@outlook.com>
2024-06-25 12:05:09 +0100

    Implemented list of TODOs

--- src/test/java/com/github/dsmiles/bestimage/BestImageApplicationTest.java
+++ src/test/java/com/github/dsmiles/bestimage/BestImageApplicationTest.java
@@ -1,13 +1,30 @@
 package com.github.dsmiles.bestimage;
 
+import com.github.dsmiles.bestimage.controller.BestImageController;
+import com.github.dsmiles.bestimage.repository.BestImageRepository;
+import com.github.dsmiles.bestimage.service.BestImageService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BestImageApplicationTest {
 
+    @Autowired
+    private BestImageController bestImageController;;
+
+    @Autowired
+    private BestImageService bestImageService;
+
+    @Autowired
+    private BestImageRepository bestImageRepository;
+
     @Test
     void contextLoads() {
+        assertNotNull(bestImageController);
+        assertNotNull(bestImageService);
+        assertNotNull(bestImageRepository);
     }
-
 }
